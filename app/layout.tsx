23a39e08d57f31dbf899569a4b831fd182dd9eed
Alex Diehl <alexander.s.diehl@gmail.com>
2024-06-17 16:51:17 -0700

    secureli install & init & scan

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Alex the Diehl-io",
+  title: 'Alex the Diehl-io',
   description: "Alex Diehl's personal website",
 };
 
