f9fca06a4b4331ab1b9a72513b202a7cbb994ae2
Alex Diehl <alexander.s.diehl@gmail.com>
2024-06-17 18:06:20 -0700

    Add basic test for live site

---
+++ scripts/e2e.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Fail script if any command returns a non-zero exit code
+set -e
+
+# Define domain to build URL
+domain=$(cat ./CNAME)
+
+# Define pages to request
+pages=$(ls -d app/*/ | cut -f2 -d '/')
+
+echo "Test page endpoints"
+
+for page in $(echo $pages)
+do
+  $url="https://$domain/$page"
+  return_code=$(curl -s -o /dev/null -w "%{http_code}" $url)
+  echo "/$page returned $return_code"
+  if [ $return_code != "200" ]
+  then
+    exit 1
+  fi
+done
